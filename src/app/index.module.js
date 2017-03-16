f62fc976cab9e1a0a2b7e04257e6b1e54ffa1a88
EnhengBenben <983001487@qq.com>
2017-03-16 17:47:06 +0800

    格式化module

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,16 @@
   'use strict';
 
   angular
-    .module('ngDemo', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
-
+    .module('ngDemo',
+        [
+            'ngAnimate',
+            'ngCookies',
+            'ngSanitize',
+            'ngMessages',
+            'ngAria',
+            'ngResource',
+            'ui.router',
+            'ngMaterial',
+            'toastr'
+        ]);
 })();
