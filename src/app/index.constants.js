f571ea75f772f8919e22e3100150317def119997
EnhengBenben <983001487@qq.com>
2017-03-16 15:06:05 +0800

    Project init

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('ngDemo')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
